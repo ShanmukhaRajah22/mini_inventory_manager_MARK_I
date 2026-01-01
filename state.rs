b972c1a8d783040e06801442df296d09918d5f61
ShanmukhaRajah22 <shanmukharajah22@gmail.com>
2026-01-01 21:50:05 +0530

    Remove duplicate Rust files from repo root

--- state.rs
+++
@@ -1,8 +0,0 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}};
-use crate::models::Item;
-
-pub type DB = Arc<Mutex<HashMap<String, Item>>>;
-
-pub fn init_db() -> DB {
-    Arc::new(Mutex::new(HashMap::new()))
-}
